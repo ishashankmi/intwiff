8011d2201f2542c549e2a81805ef6b803cec3df5
codydeny <codydeny@gmail.com>
2020-05-26 16:05:02 +0530

    Updates

---
+++ precache-manifest.68a5141947f97c7a8e606e42bb94f26f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fb5681be61b826875e26442a3e0437",
+    "url": "/intwiff/index.html"
+  },
+  {
+    "revision": "490982286ed5704b4046",
+    "url": "/intwiff/static/css/3.9618f9c4.chunk.css"
+  },
+  {
+    "revision": "d37a71f9b2a023afdab3",
+    "url": "/intwiff/static/css/main.387c7524.chunk.css"
+  },
+  {
+    "revision": "c3c7f38cd51afe24ce93",
+    "url": "/intwiff/static/js/2.4ca72337.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/intwiff/static/js/2.4ca72337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490982286ed5704b4046",
+    "url": "/intwiff/static/js/3.1389107d.chunk.js"
+  },
+  {
+    "revision": "d37a71f9b2a023afdab3",
+    "url": "/intwiff/static/js/main.c69abe71.chunk.js"
+  },
+  {
+    "revision": "03c237880b49e6b7933e",
+    "url": "/intwiff/static/js/runtime-main.c862b5b7.js"
+  }
+]);
