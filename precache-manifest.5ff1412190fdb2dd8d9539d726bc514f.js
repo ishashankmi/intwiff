2de0ab505b57c1dd2fc7f0da8ee256778c52c157
codydeny <codydeny@gmail.com>
2020-05-31 16:50:19 +0530

    Updates

---
+++ precache-manifest.5ff1412190fdb2dd8d9539d726bc514f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4f22f525c2b98b49cff63f0614baab",
+    "url": "/intwiff/index.html"
+  },
+  {
+    "revision": "abb0f305b42fa1ec9250",
+    "url": "/intwiff/static/css/2.34dcc6c8.chunk.css"
+  },
+  {
+    "revision": "8364eb2948a0f7dba0f1",
+    "url": "/intwiff/static/css/3.920aa423.chunk.css"
+  },
+  {
+    "revision": "6ca0c9ce94836b16884a",
+    "url": "/intwiff/static/css/main.31279ca1.chunk.css"
+  },
+  {
+    "revision": "abb0f305b42fa1ec9250",
+    "url": "/intwiff/static/js/2.9875e841.chunk.js"
+  },
+  {
+    "revision": "f722fbe153d774a75fece87dba9e2b89",
+    "url": "/intwiff/static/js/2.9875e841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8364eb2948a0f7dba0f1",
+    "url": "/intwiff/static/js/3.26166c71.chunk.js"
+  },
+  {
+    "revision": "0958985350d69c5042c9",
+    "url": "/intwiff/static/js/4.3dfe1ed0.chunk.js"
+  },
+  {
+    "revision": "63ab52b265d8037b7a859e329f6fecca",
+    "url": "/intwiff/static/js/4.3dfe1ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca0c9ce94836b16884a",
+    "url": "/intwiff/static/js/main.234f5a19.chunk.js"
+  },
+  {
+    "revision": "eae9141de31e135fa32b",
+    "url": "/intwiff/static/js/runtime-main.b21fe58f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/intwiff/precache-manifest.42641ebf50848202775582ff1fb0cc84.js"
+  "/intwiff/precache-manifest.5ff1412190fdb2dd8d9539d726bc514f.js"
 );
 
 self.addEventListener('message', (event) => {
